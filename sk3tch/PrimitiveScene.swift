62721736b7bb12e16cc7256cf7969d985cac492d
Jessica Mann <jmann@smith.edu>
2014-12-18 02:13:25 -0500

    Added code for making lines - it's currently commented out since it doesn't work. Nothing else is modified.

--- sk3tch/PrimitiveScene.swift
+++ sk3tch/PrimitiveScene.swift
@@ -25,6 +25,32 @@ class PrimitiveScene: SCNScene {
     func addPoints(points : [SCNNode]){
         self.points = points
         
+        // drawing a line between each of the two points
+//        var point1 : SCNVector3
+//        var point2 : SCNVector3
+//        var vertices : [SCNVector3]
+//        var geoSrc : SCNGeometrySource
+//        var idx : [Int32] = [0,1]
+//        var data : NSData
+//        var geoElements: SCNGeometryElement
+//        for index in 0...self.points.count-2 {
+//            point1 = points[index].position
+//            point2 = points[index+1].position
+//            vertices = [point1, point2]
+//            geoSrc = SCNGeometrySource(vertices: UnsafePointer<SCNVector3>(vertices), count: vertices.count)
+//            
+//            //index buffer
+//            data = NSData(bytes: idx, length: (sizeof(Int32) * idx.count))
+//            geoElements = SCNGeometryElement(data: data, primitiveType: SCNGeometryPrimitiveType.Line, primitiveCount: idx.count, bytesPerIndex: sizeof(Int32))
+//            
+//            // line node
+//            let geo = SCNGeometry(sources: [geoSrc], elements: [geoElements])
+//            let line = SCNNode(geometry: geo)
+//            geo.firstMaterial?.diffuse.contents = UIColor.orangeColor()
+//            self.rootNode.addChildNode(line)
+//        }
+        
+        
         var sphereGeometry : SCNSphere
         var sphereNode : SCNNode
         
